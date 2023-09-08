dfced9cd147c3b1a24d9dd5e0c63cd14dcb5db6a
Xu Brian <xubrian@vlan-2655-10-19-199-98.student.wireless.sydney.edu.au>
2023-09-08 13:57:59 +1000

    Added my project

---
+++ src/test/java/elec5619/sydney/edu/au/mental_health_support_website/MentalHealthSupportWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package elec5619.sydney.edu.au.mental_health_support_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthSupportWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
