013673c6bb8164adcfd2eabd52b2c35ec218bdb7
Xu Brian <xubrian@Xus-MacBook-Air.local>
2023-09-15 14:56:23 +1000

    add entity

---
+++ backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/db/entities/Thread.java
@@ -0,0 +1,33 @@
+package elec5619.sydney.edu.au.mental_health_support_website.db.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+enum ThreadTag {
+    ANXIETY,
+    DEPRESSION,
+    GENERAL,
+    DISCUSSION
+}
+
+@Entity
+@Getter
+@Setter
+public class Thread {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String title;
+    private String content;
+    private List<ThreadTag> tags = new ArrayList<>();
+    private Long authorID;
+    private Date timestamp;
+}
--- backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/db/repository/UserRepository.java
+++ backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/db/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package elec5619.sydney.edu.au.mental_health_support_website.db.models.repository;
+package elec5619.sydney.edu.au.mental_health_support_website.db.repository;
 
-import elec5619.sydney.edu.au.mental_health_support_website.db.models.User;
+import elec5619.sydney.edu.au.mental_health_support_website.db.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
