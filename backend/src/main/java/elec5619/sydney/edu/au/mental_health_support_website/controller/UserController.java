c6862fb1e00093dc392e328c83378be3afff3a20
shxu4776 <shxu4776@uni.sydney.edu.au>
2023-10-13 14:55:33 +1100

    change request

--- backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/controller/UserController.java
+++ backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/controller/UserController.java
@@ -67,9 +67,9 @@ public class UserController {
     }
 
     @PostMapping("register")
-    public String register(@RequestParam String email,
-                         @RequestParam String username,
-                         @RequestParam String password) throws IOException, InterruptedException {
+    public String register(@RequestBody String email,
+                         @RequestBody String username,
+                         @RequestBody String password) throws IOException, InterruptedException {
         if(Strings.isEmpty(email) || Strings.isEmpty(username) || Strings.isEmpty(password)) {
             return ErrorsEnum.PARAMETER_ERROR.getErrorMessage();
         }
@@ -88,8 +88,8 @@ public class UserController {
 
     @PostMapping("login")
     public String login(
-            @RequestParam String email,
-            @RequestParam String password) {
+            @RequestBody String email,
+            @RequestBody String password) {
         if(Strings.isEmpty(email) || Strings.isEmpty(password)) {
             return ErrorsEnum.PARAMETER_ERROR.getErrorMessage();
         }
@@ -102,7 +102,8 @@ public class UserController {
     }
 
     @GetMapping("followers")
-    private List<Users> getFollowers(@RequestParam("token") String token) {
+    private List<Users> getFollowers(@RequestHeader("token") String token) {
+
         String username = TokenUtil.getUsernameFromToken(token);
         if(username == null || username.length() == 0) {
             return new ArrayList<>();
@@ -111,7 +112,7 @@ public class UserController {
     }
 
     @GetMapping("followed")
-    private List<Users> getFollowed(@RequestParam("token") String token) {
+    private List<Users> getFollowed(@RequestHeader("token") String token) {
         String username = TokenUtil.getUsernameFromToken(token);
         if(username == null || username.length() == 0) {
             return new ArrayList<>();
@@ -120,43 +121,43 @@ public class UserController {
     }
 
     @PostMapping("follow")
-    private boolean follow(@RequestParam("token") String token, String toUsername) {
+    private boolean follow(@RequestHeader("token") String token, String toUsername) {
         String fromUsername = TokenUtil.getUsernameFromToken(token);
         return userService.follow(fromUsername, toUsername);
     }
 
     @PostMapping("mute")
-    private boolean mute(@RequestParam("token") String token, String toUsername) {
+    private boolean mute(@RequestHeader("token") String token, String toUsername) {
         String fromUsername = TokenUtil.getUsernameFromToken(token);
         return userService.mute(fromUsername, toUsername);
     }
 
     @PostMapping("updateProfile")
-    private Users updateProfile(@RequestParam("token") String token,
-                            @RequestParam String newUsername,
-                           @RequestParam String newEmail,
-                           @RequestParam String newPhoneNumber,
-                           @RequestParam String newBirthday) {
+    private Users updateProfile(@RequestHeader("token") String token,
+                            @RequestBody String newUsername,
+                           @RequestBody String newEmail,
+                           @RequestBody String newPhoneNumber,
+                           @RequestBody String newBirthday) {
         String userName = TokenUtil.getUsernameFromToken(token);
         return userService.updateProfile(userName, newUsername, newEmail, newPhoneNumber, newBirthday);
     }
 
     @PostMapping("updatePassword")
-    private boolean updatePassword(@RequestParam("token") String token,
-                        @RequestParam String oldPassword,
-                        @RequestParam String newPassword) throws IOException, InterruptedException {
+    private boolean updatePassword(@RequestHeader("token") String token,
+                        @RequestBody String oldPassword,
+                        @RequestBody String newPassword) throws IOException, InterruptedException {
         String userName = TokenUtil.getUsernameFromToken(token);
         return userService.updatePassword(userName, oldPassword, newPassword);
     }
 
     @GetMapping("profile")
-    private Users getProfile(@RequestParam("token") String token) {
+    private Users getProfile(@RequestHeader("token") String token) {
         String userName = TokenUtil.getUsernameFromToken(token);
         return userService.getUserByUsername(userName);
     }
 
     @GetMapping("searchUser")
-    private Users searchUser(@RequestParam String userName) {
+    private Users searchUser(@RequestBody String userName) {
         return userService.getUserByUsername(userName);
     }
 }
