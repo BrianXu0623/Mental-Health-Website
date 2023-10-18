f65e7dadb0bc4acf47b7e863200aa33c8084d325
SizhuangKang <74434894+SizhuangKang@users.noreply.github.com>
2023-10-19 01:12:22 +1100

    added CROS to all controllers

--- backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/controller/ThreadController.java
+++ backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/controller/ThreadController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/threads/")
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class ThreadController {
     @Autowired
     private ThreadCommentService threadCommentService;
