0837a9fed8c558d0e3cecb0eba23d5e88a69cccc
shxu4776 <shxu4776@uni.sydney.edu.au>
2023-10-19 18:26:14 +1100

    all

--- backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/MentalHealthSupportWebsiteApplication.java
+++ backend/src/main/java/elec5619/sydney/edu/au/mental_health_support_website/MentalHealthSupportWebsiteApplication.java
@@ -3,12 +3,10 @@ package elec5619.sydney.edu.au.mental_health_support_website;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class MentalHealthSupportWebsiteApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(MentalHealthSupportWebsiteApplication.class, args);
     }
 
