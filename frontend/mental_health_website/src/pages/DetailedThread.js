ea94f5b7ce2b5d033908b219fedf35d46877c72a
shxu4776 <shxu4776@uni.sydney.edu.au>
2023-10-28 21:21:24 +1100

    merge

--- frontend/mental_health_website/src/pages/DetailedThread.js
+++ frontend/mental_health_website/src/pages/DetailedThread.js
@@ -3,11 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import './DetailedThread.css';
 
 function DetailedThread() {
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 8c0fb0f167b85eefe404d76906677510e2a5b5fe
     const { id } = useParams();
     const [data, setData] = useState([]);
 
@@ -33,28 +29,17 @@ function DetailedThread() {
                 <p>{data?.thread?.content}</p>
             </div>
 
-<<<<<<< HEAD
             <div className="actions">
                 <Link to={`/addcomment/${data?.thread?.id}`} className="comment-link">
                     + Add a comment
                 </Link>
-                {/*<button className="favorite-button" onClick={favorite_thread}>*/}
-                {/*    Favorite*/}
-                {/*</button>*/}
-=======
-            
-
-            <div className="buttons">
-                <Link to={`/addcomment/${data?.thread?.id}`}>+ Add a comment</Link>
-                
->>>>>>> 8c0fb0f167b85eefe404d76906677510e2a5b5fe
             </div>
 
             <div className="comments">
                 <div className="comments-heading">Comments:</div>
                 {data?.comments?.map(comment => (
                     <div key={comment.id} className="comment-content">
-                        <span className="comment-author">Username: {comment.commentAuthor}</span>
+                        <span className="comment-author">{comment.commentAuthor}</span>
                         <span> - {comment.comment}</span>
                     </div>
                 ))}
